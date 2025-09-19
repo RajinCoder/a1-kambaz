0086c2747c475226d1b567640137066b0a4b191a
pmoise <petercmoise@gmail.com>
2025-09-19 19:12:00 -0400

    fix

--- src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -1,6 +1,8 @@
 // app/(Kambaz)e/Courses/[cid]/Assignments/[aid]/page.tsx
 import Editor from "./Editor";
 
+export const dynamic = "force-dynamic";
+
 export default function AssignmentPage() {
   return <Editor />;
 }
