efea9f912d5ed0b6a426d604035856cec09e18c5
pmoise <petercmoise@gmail.com>
2025-09-19 19:21:30 -0400

    capital I?

--- src/app/(Kambaz)/Account/Navigation.tsx
+++ src/app/(Kambaz)/Account/Navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation">
-      <Link href="Signin"> Signin </Link> <br />
+      <Link href="SignIn"> Signin </Link> <br />
       <Link href="Signup"> Signup </Link> <br />
       <Link href="Profile"> Profile </Link> <br />
     </div>
