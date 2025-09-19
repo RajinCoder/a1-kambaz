4a5675bdccd8e182f064957012b17a365b228038
pmoise <petercmoise@gmail.com>
2025-09-19 19:07:18 -0400

    m

--- src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -1,10 +1,6 @@
 // app/(Kambaz)e/Courses/[cid]/Assignments/[aid]/page.tsx
 import Editor from "./Editor";
 
-export default function AssignmentPage({
-  params,
-}: {
-  params: { cid: string; aid: string };
-}) {
-  return <Editor courseId={params.cid} assignmentId={params.aid} />;
+export default function AssignmentPage() {
+  return <Editor />;
 }
